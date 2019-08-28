c014a330ba87d706b59d0058b74dacb61cd1d350
Salomé Pelaez <spsalome@gmail.com>
2019-08-28 15:20:43 -0500

    Canvas counter added

--- Survival3/Assets/Scripts/Manager.cs
+++ Survival3/Assets/Scripts/Manager.cs
@@ -2,72 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using NPC.Enemy;
+using NPC.Ally;
 
-namespace NPC
+
+public class Manager : MonoBehaviour
 {
-    public class Manager : MonoBehaviour
+    public const int maxGen = 25;
+    public readonly int minGen;
+
+    public Text ZombiesNum;
+    public Text VillagersNum;
+
+    public static float sHero;
+
+    public void Awake()
     {
-        public const int maxGen = 25;
-        public readonly int minGen;
+        sHero = Random.Range(0.1f, 0.2f);
+    }
 
-        public static float sHero;
-
-        public void Awake()
+    void Start()
+    {            
+        int rnd = Random.Range(minGen, maxGen);
+        for (int j = 0; j < rnd; j++)
         {
-            sHero = Random.Range(0.1f, 0.2f);
-        }
+            new minGenerator(minGen);
+            GameObject thePeople = GameObject.CreatePrimitive(PrimitiveType.Cube); // El GameObject "thePeople" genera los cubos para zombies, aldeanos y héroes.
 
-        void Start()
-        {            
-            int rnd = Random.Range(minGen, maxGen);
-            for (int j = 0; j < rnd; j++)
+            // El Vector3 de posición es el que servirá para generar los cubos en una posición aleatoria.
+            Vector3 posicion = new Vector3();
+            posicion.x = Random.Range(-30, 30);
+            posicion.z = Random.Range(-30, 30);
+            thePeople.transform.position = posicion; // A los cubos se les asigna la posición aleatoria antes mencionada.
+            thePeople.AddComponent<Rigidbody>(); // Se les agrega Rigidbody.
+            thePeople.GetComponent<Rigidbody>().freezeRotation = true;
+            int change = Random.Range(0, 2); // Se creó un Random con dos únicas opciones.
+
+            // El siguiente bloque de código se encarga de generar el héroe, está separado pues a diferencia de los miembros de la aldea, solo debe ser creado una vez.
+            if (j == 0)
             {
-                new minGenerator(minGen);
-                GameObject thePeople = GameObject.CreatePrimitive(PrimitiveType.Cube); // El GameObject "thePeople" genera los cubos para zombies, aldeanos y héroes.
+                thePeople.AddComponent<Hero>(); // Se le agregan los componentes de la clase Hero.
+                thePeople.AddComponent<HeroAim>(); // Igualmente se le agregan los componentes de HeroAim.
+                thePeople.GetComponent<Renderer>().material.color = Color.black; // Se le asignó color negro para diferenciarlos de otros objetos.
+            }
 
-                // El Vector3 de posición es el que servirá para generar los cubos en una posición aleatoria.
-                Vector3 posicion = new Vector3();
-                posicion.x = Random.Range(-30, 30);
-                posicion.z = Random.Range(-30, 30);
-                thePeople.transform.position = posicion; // A los cubos se les asigna la posición aleatoria antes mencionada.
-                thePeople.AddComponent<Rigidbody>(); // Se les agrega Rigidbody.
-                thePeople.GetComponent<Rigidbody>().freezeRotation = true;
-                int change = Random.Range(0, 2); // Se creó un Random con dos únicas opciones.
-
-                // El siguiente bloque de código se encarga de generar el héroe, está separado pues a diferencia de los miembros de la aldea, solo debe ser creado una vez.
-                if (j == 0)
+            else
+            {
+                switch (change) // La primera posibilidad del Random genera los zombies, la segunda los aldeanos.
                 {
-                    thePeople.AddComponent<Hero>(); // Se le agregan los componentes de la clase Hero.
-                    thePeople.AddComponent<HeroAim>(); // Igualmente se le agregan los componentes de HeroAim.
-                    thePeople.GetComponent<Renderer>().material.color = Color.black; // Se le asignó color negro para diferenciarlos de otros objetos.
-                }
-
-                else
-                {
-                    switch (change) // La primera posibilidad del Random genera los zombies, la segunda los aldeanos.
-                    {
-                        case 0:
-                            thePeople.AddComponent<NPC.Enemy.Zombie>(); // Se agregan los componentes de su respectiva clase.
-                            break;
-                        case 1:
-                            thePeople.AddComponent<NPC.Ally.Villagers>(); // Se agregan los componentes de su respectiva clase. 
-                            break;
-                    }
+                    case 0:
+                        thePeople.AddComponent<Zombie>(); // Se agregan los componentes de su respectiva clase.
+                        break;
+                    case 1:
+                        thePeople.AddComponent<Villagers>(); // Se agregan los componentes de su respectiva clase. 
+                        break;
                 }
             }
+
+            int v = 0;
+            int z = 0;
+                
+            foreach (Zombie zombie in Transform.FindObjectsOfType<Zombie>())
+            {
+                z = z + 1;
+                ZombiesNum.text = "Zombies: " + z;
+            }
+
+            foreach (Villagers villagers in Transform.FindObjectsOfType<Villagers>())
+            {
+                v = v + 1;
+                VillagersNum.text = "Villagers: " + v;
+            }
         }
+    }
 
-        
-    }   
+}   
 
-    public class minGenerator
+public class minGenerator
+{
+    int minGen = Random.Range(5, 15);
+    public minGenerator(int g)
     {
-        int minGen = Random.Range(5, 15);
-        public minGenerator(int g)
-        {
-            this.minGen = g;
-            minGen = Random.Range(5, 15);
-        }
+        this.minGen = g;
+        minGen = Random.Range(5, 15);
     }
 }
 
